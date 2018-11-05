0b87bfa3e22cd6925b119c2f1176c3d63509ee2c
Markus Gay <mgay@jobcase.com>
2018-11-05 15:55:42 -0500

    Create output folder prior launching jvm with profiler

--- dev-ops/jenkins/benchmarks/script/benchmark-servers-start.sh
+++ dev-ops/jenkins/benchmarks/script/benchmark-servers-start.sh
@@ -171,10 +171,14 @@ do
     then
         mkdir -p ${LOGS_DIR}
 
+        mkdir -p ${OUTPUT_FOLDER#--outputFolder }
+
         nohup ${SCRIPT_DIR}/benchmark-bootstrap.sh ${CONFIG_PRM} "--config" ${CONFIG_INCLUDE} "--logsFolder" ${LOGS_DIR} \
         "--remoteuser" ${REMOTE_USER} "--remoteHostName" ${host_name} > ${file_log} 2>& 1 &
     else
         ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no ${REMOTE_USER}"@"${host_name} mkdir -p ${LOGS_DIR}
+
+        ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no ${REMOTE_USER}"@"${host_name} mkdir -p ${OUTPUT_FOLDER#--outputFolder }
 
         ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no ${REMOTE_USER}"@"${host_name} \
             "JAVA_HOME='${JAVA_HOME}'" \
